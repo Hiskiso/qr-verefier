ef3d1b937f9713950a1b1dd3828a4a99e188e627
Hiskiso <roma123qq@gmail.com>
2025-05-30 04:00:37 +0400

    Осталось подключить апи

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import {
+  ConfigProvider,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  View,
+  Panel,
+  PanelHeader,
+  Group,
+  Gradient,
+  Placeholder,
+  Button,
+  Spinner 
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { Avatar } from "antd";
+import { useState } from "react";
+
+const App = () => {
+
+  const [userData, setUserData] = useState({})
+  const [isLoadingMember, setIsLoadingMember] = useState(false)
+  const [isFirstScan, setIsFirstScan] = useState(true)
+
+  function onScan(qrData){
+    let qrString = atob(qrData[0].rawValue)
+    let parsedData = JSON.parse(qrString)
+    console.log(parsedData)
+    // setIsLoadingMember(true)
+    // fetch("https://klenrest.ru/vkbot/users/getUser",
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({user_id: parsedData._id})
+    //   }
+    // )
+    // .then(r=>r.json())
+    // .then(data=> {setUserData(data)})
+  }
+
+  function onSubmitFest(){
+     setIsLoadingMember(true)
+  //  fetch("https://klenrest.ru/vkbot/users/getUser",
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({user_id: parsedData._id})
+    //   }
+    // )
+    // .then(r=>r.json())
+    // .then(data=> {setUserData(data)})
+  }
+
+  return (
+    <ConfigProvider colorScheme="dark">
+      <AppRoot>
+        <SplitLayout>
+          <SplitCol autoSpaced>
+
+            <View id="main" activePanel="main">
+              <Panel id="main">
+                <PanelHeader>Подтверждение участия</PanelHeader>
+                <Group style={{}}>
+                  <Gradient mode="tint" to="top">
+                    {isFirstScan ? <Placeholder>Отсканируйте код участника форума</Placeholder> :<Placeholder
+                      icon={isLoadingMember? <Spinner/> :  <Avatar size={96} />}
+                      title={isLoadingMember? <Spinner/> : "Алексей Мазелюк"}
+                      
+                      action={
+                        <Button disabled={isLoadingMember} size="m" mode="primary" onClick={()=>{onSubmitFest()}}>
+                          Подтвердить участие
+                        </Button>
+                      }
+                    >
+                      {isLoadingMember ? <Spinner/> : "Участник не подтвержден"}
+                    </Placeholder>}
+                  </Gradient>
+
+
+                  <Scanner
+                    onScan={(result) => {
+
+                      // console.log(result);
+                      setIsFirstScan(false)
+                      onScan(result)
+                    }}
+                    sound={false}
+                    styles={{ container: { border: "unset" } }}
+                  />
+
+  
+
+                </Group>
+              </Panel>
+            </View>
+          </SplitCol>
+        </SplitLayout>
+      </AppRoot>
+    </ConfigProvider>
+  );
+};
+
+export default App;
